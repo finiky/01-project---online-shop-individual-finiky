ca17e9b157b9a3b52a5d2540b3aeab14d1a50442
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-03-30 10:37:36 +1300

    wrote getProducts function to fetch the products from stripe API

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -2,9 +2,8 @@ const getProducts = async () => {
   // TODO Fetch products and prices from the Stripe API
   // Use the following URL for your fetch request
   const url = `${process.env.REACT_APP_API_URL}/products`;
-
-  const products = [];
-
+  const response  = await fetch(url);
+  const products = await response.json();
   return products;
 };
 

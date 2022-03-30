3c5664b9b769574fc269ffd3bb628c536ed7dc89
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-03-31 00:07:06 +1300

    rederd products on the  app

--- src/App.js
+++ src/App.js
@@ -16,25 +16,49 @@ function App() {
 
     loadData();
   }, []);
-  let usefulInfo = products.map((product) => {
-    const data = {
-      name: product.name,
-      description: product.description,
-      image: product.images[0],
-      category: product.metadata.category,
-      price: product.prices[0].unit_amount
-    };
-    return data;
-  });
-  for ( let i = 0; i < usefulInfo.length; i++) {
-    usefulInfo[i].price = parseFloat(usefulInfo[i].price/100); 
-  }
+
+  const Product = ({ name, description, image, price }) => {
+    return (
+      <li className="product">
+        <div className="imageContainer">
+          <img className="productImage" src={image} alt={name}></img>
+        </div>
+        <div>
+          <h2 className="productName">{name}</h2>
+          <p className="productDescription">{description}</p>
+        </div>
+        <p className="productPrice">{price}</p>
+        <BuyNow />
+      </li>
+    );
+  };
+  const AllProducts = ({ products }) => {
+    return (
+      <ul className="productList">
+        {products.map((product) => {
+          for (let i = 0; i < products.length; i++) {
+            product.price = parseFloat(product.prices[0].unit_amount / 100);
+            product.price = product.price.toString();
+          }
+          return (
+            <Product
+              key={product.id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              image={product.images[0]}
+            />
+          );
+        })}
+      </ul>
+    );
+  };
   return (
     <div className="container">
       <h1>Screen Time</h1>
       <p>An Online Place To Buy Monitors</p>
+      <AllProducts products={products} />
     </div>
   );
 }
-
 export default App;
--- src/BuyNow.js
+++ src/BuyNow.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { checkout } from "./services/checkout";
-
+//import { getProducts } from "./services/getProducts";
 const BuyNow = () => {
-  const onClick = () => {
-    checkout(); // Need to add a price ID
+  const onClick = (event) => {
+    checkout();
   };
-
   return (
     <button className="BuyNow" onClick={onClick}>
       Buy Now

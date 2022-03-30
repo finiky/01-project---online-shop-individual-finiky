d473a4f5a8baf81112bb39f48b71f742aac8ce79
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-03-30 20:57:05 +1300

    getProducts was completed to fetch the products. product.map was used to extract useful data in variable usefulInfo. Converted the price to  a floating point value.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   // use the products variable to read all of your products
   // and display them on your page
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     const loadData = async () => {
       const products = await getProducts();
@@ -17,10 +16,23 @@ function App() {
 
     loadData();
   }, []);
-
+  let usefulInfo = products.map((product) => {
+    const data = {
+      name: product.name,
+      description: product.description,
+      image: product.images[0],
+      category: product.metadata.category,
+      price: product.prices[0].unit_amount
+    };
+    return data;
+  });
+  for ( let i = 0; i < usefulInfo.length; i++) {
+    usefulInfo[i].price = parseFloat(usefulInfo[i].price/100); 
+  }
   return (
     <div className="container">
-      <h1>Sunglass Shop</h1>
+      <h1>Screen Time</h1>
+      <p>An Online Place To Buy Monitors</p>
     </div>
   );
 }
